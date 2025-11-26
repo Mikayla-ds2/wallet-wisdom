b223d4c0152388fb326f9e4385c696fbdeb32c12
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-11-26 11:18:23 -0500

    doing more stuff

--- script.sql
+++ script.sql
@@ -150,6 +150,96 @@ create table if not exists financialMetrics (
     Foreign Key (personID) REFERENCES demographics(personID)
 )
 
+-- wanna create a full table too with all features --
+drop table if exists finance
+
+CREATE TABLE finance (
+    personID int generated always as identity primary key,
+    age INT,
+    gender TEXT,
+    raceEthnicity TEXT,
+    education TEXT,
+    region TEXT,
+    householdType TEXT,
+    householdSize INT,
+    career TEXT,
+    workArrangement TEXT,
+    annualIncome DECIMAL(10, 2),
+    sideHustleIncome DECIMAL(10, 2),
+    monthlyIncome DECIMAL(10, 2),
+    housingStatus TEXT,
+    housingCost DECIMAL(10, 2),
+    propertyTax DECIMAL(10, 2),
+    hoaFees DECIMAL(10, 2),
+    homeInsurance DECIMAL(10, 2),
+    utilities DECIMAL(10, 2),
+    internet DECIMAL(10, 2),
+    phone DECIMAL(10, 2),
+    ownsCar BOOLEAN,
+    carPayment DECIMAL(10, 2),
+    carInsurance DECIMAL(10, 2),
+    gas DECIMAL(10, 2),
+    publicTransit DECIMAL(10, 2),
+    carMaintenance DECIMAL(10, 2),
+    healthInsurance DECIMAL(10, 2),
+    oopMedical DECIMAL(10, 2),
+    dentalVision DECIMAL(10, 2),
+    lifeInsurance DECIMAL(10, 2),
+    studentLoans DECIMAL(10, 2),
+    ccPayment DECIMAL(10, 2),
+    personalLoans DECIMAL(10, 2),
+    medicalDebt DECIMAL(10, 2),
+    groceries DECIMAL(10, 2),
+    diningOut DECIMAL(10, 2),
+    coffee DECIMAL(10, 2),
+    alcohol DECIMAL(10, 2),
+    streaming DECIMAL(10, 2),
+    musicStreaming DECIMAL(10, 2),
+    gaming DECIMAL(10, 2),
+    gym DECIMAL(10, 2),
+    otherSubscriptions DECIMAL(10, 2),
+    clothing DECIMAL(10, 2),
+    personalCare DECIMAL(10, 2),
+    householdSupplies DECIMAL(10, 2),
+    childcare DECIMAL(10, 2),
+    petExpenses DECIMAL(10, 2),
+    entertainment DECIMAL(10, 2),
+    hobbies DECIMAL(10, 2),
+    travel DECIMAL(10, 2),
+    gifts DECIMAL(10, 2),
+    donations DECIMAL(10, 2),
+    retirement401k DECIMAL(10, 2),
+    iraContribution DECIMAL(10, 2),
+    emergencyFundContributions DECIMAL(10, 2),
+    generalSavings DECIMAL(10, 2),
+    investmentContributions DECIMAL(10, 2),
+    contributions529 DECIMAL(10, 2),
+    totalExpenses DECIMAL(10, 2),
+    totalSavingsInvestments DECIMAL(10, 2),
+    monthlyCashFlow DECIMAL(10, 2),
+    housingRatio DECIMAL(10, 2),
+    debtToIncome DECIMAL(10, 2),
+    savingsInvestmentsRate DECIMAL(10, 2),
+    carPaymentRatio DECIMAL(10, 2),
+    monthsSaved DECIMAL(10, 2),
+    financialHealth TEXT,
+    age_category TEXT
+);
+
+/copy finance(age, gender, raceEthnicity, education, region, householdType, 
+householdSize, career, workArrangement, annualIncome, sideHustleIncome, monthlyIncome, 
+housingStatus, housingCost, propertyTax, hoaFees, homeInsurance, utilities, internet, phone, 
+ownsCar, carPayment, carInsurance, gas, publicTransit, carMaintenance, healthInsurance, oopMedical, 
+dentalVision, lifeInsurance, studentLoans, ccPayment, personalLoans, medicalDebt, groceries, diningOut, 
+coffee, alcohol, streaming, musicStreaming, gaming, gym, otherSubscriptions, clothing, personalCare, 
+householdSupplies, childcare, petExpenses, entertainment, hobbies, travel, gifts, donations, retirement401k, 
+iraContribution, emergencyFundContributions, generalSavings, investmentContributions, contributions529, 
+totalExpenses, totalSavingsInvestments, monthlyCashFlow, housingRatio, debtToIncome, 
+savingsInvestmentsRate, carPaymentRatio, monthsSaved, financialHealth, age_category)
+from '/tmp/personalFinanceDataset.csv'
+DELIMITER ','
+csv header;
+
 /copy demographics(personID, age, gender, raceEthnicity, education, region, householdType, householdSize)
 from '/tmp/demographics.csv'
 DELIMITER ','
@@ -230,6 +320,83 @@ join debt on demographics.personID = debt.personID
 where studentLoans > 0
 order by studentLoans desc
 limit 20;
--- note: explore person 2425
+-- note: explore person 2425 --
 
 
+select * from finance
+
+select * from finance
+where monthlycashflow > 0
+order by monthlycashflow desc;
+
+select education, sum(monthlycashflow) as sum from finance
+where monthlycashflow > 0
+group by education
+order by sum desc;
+
+select education, sum(travel) as travelExpenses from finance
+where householdSize < 3
+group by education
+order by travelExpenses desc
+
+select financialHealth, sum(travel) as travelExpenses from finance
+where householdSize < 3
+group by financialHealth
+order by travelExpenses desc;
+
+select * from finance
+
+select financialhealth, sum(totalsavingsinvestments) as total from finance
+where householdSize < 3
+group by financialhealth
+order by total desc;
+
+select raceEthnicity, count(raceEthnicity) as count from finance
+where financialHealth = 'FinanciallyThriving'
+and householdSize < 3
+group by raceEthnicity
+order by count desc;
+
+select region, count(region) as count from finance
+where monthlycashflow > 0
+group by region
+order by count desc;
+
+select householdType, count(householdType) as count from finance
+where monthlycashflow > 0
+group by householdType
+order by count desc;
+
+select * from finance
+where raceEthnicity = 'Black/African American'
+and monthlycashflow > 0
+order by annualIncome desc;
+
+select career, householdType, financialHealth, count(financialHealth) as count from finance
+where monthlycashflow > 0
+group by career, householdType, financialHealth
+order by count desc;
+
+select personID, age, gender, raceEthnicity, education, region, householdType, householdSize, career, workArrangement, annualIncome, housingStatus, monthlyCashFlow, financialHealth from finance
+where career = 'Engineering'
+and financialHealth not in ('FinanciallyThriving', 'FinanciallyStable')
+order by monthlyCashFlow desc;
+
+select personID, age, gender, raceEthnicity, education, region, householdType, householdSize, career, workArrangement, annualIncome, housingStatus, monthlyCashFlow, financialHealth from finance
+where career = 'Engineering'
+and financialHealth not in ('FinanciallyThriving', 'FinanciallyStable')
+
+select * from demographics
+
+select personID, age, gender, raceEthnicity, education, region, householdType, householdSize, career, workArrangement, housingStatus, ownsCar, financialHealth from finance
+order by age desc;
+
+select personID, age, gender, raceEthnicity, education, region, householdType, householdSize, career, workArrangement, ownsCar, housingStatus, financialHealth from finance
+
+select householdType, avg(age) as averageAge from finance
+group by householdType
+order by averageAge desc;
+
+select age from finance
+order by age desc;
+-- 25 to 70 years old --
